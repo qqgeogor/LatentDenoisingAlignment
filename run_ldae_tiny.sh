c76208e08ad95a6ef2926b9a5b2dcfaa7b43e7e2
qqgeogor <308456341@qq.com>
2025-03-22 02:32:05 +0800

    a

--- run_ldae_tiny.sh
+++ run_ldae_tiny.sh
@@ -1,5 +1,5 @@
-# python train_vit_ldae_dino.py --use_amp --output_dir ../../autodl-tmp/output_tiny/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train'
+python train_vit_ldae_dino.py --use_amp --output_dir ../../autodl-tmp/output_tiny/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train'
 
 
 
-python train_vit_ldae_dino.py --use_amp --output_dir /mnt/d/repo/vit_ldae_tiny/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '/mnt/d/datasets/tiny-imagenet-200/train'
+# python train_vit_ldae_dino.py --use_amp --output_dir /mnt/d/repo/vit_ldae_tiny/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '/mnt/d/datasets/tiny-imagenet-200/train'
