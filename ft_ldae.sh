ec5897bccfa2b2b36ab19c0660e5bd9c245c2a32
qqgeogor <308456341@qq.com>
2025-03-22 02:30:08 +0800

    a

--- ft_ldae.sh
+++ ft_ldae.sh
@@ -1,1 +1,8 @@
 python finetune_cifar_classifier_hldae_dino.py --pretrained_path ../../autodl-tmp/output/*_40.pth 
+
+
+
+# python finetune_cifar_classifier_hldae_dino.py --pretrained_path /mnt/d/repo/vit_ldae_tiny/checkpoint_epoch_10.pth --img_size 128 --patch_size 16
+
+
+python finetune_cifar_classifier_hldae_dino.py --pretrained_path ../../autodl-tmp/output_tiny/*_10.pth  --img_size 128 --patch_size 16
