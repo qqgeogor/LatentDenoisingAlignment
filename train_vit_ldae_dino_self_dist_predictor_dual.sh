c282a7b478d54f4fd52955a894fb3757915ab7a8
qqgeogor <308456341@qq.com>
2025-03-23 22:54:44 +0800

    a

---
+++ train_vit_ldae_dino_self_dist_predictor_dual.sh
@@ -0,0 +1,8 @@
+
+
+# python train_vit_ldae_dino_self_dist_predictor_dual.py     --decoder_embed_dim 192     --use_amp     --output_dir /mnt/d/repo/output/cifar_self_dist_predictor_encoder/     --img_size 32     --patch_size 4     --dataset 'cifar10'     --data_path c:/dataset     --save_freq 10 
+
+
+
+python train_vit_ldae_dino_self_dist_predictor_dual.py --decoder_embed_dim 192 --use_amp --output_dir ../../autodl-tmp/output_tiny_self_dist_predictor_dual/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train' --save_freq 10
+
