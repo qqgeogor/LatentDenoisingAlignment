c2c85a37a4e7b0cbdbd65f3dd3999e4a2362f910
qqgeogor <308456341@qq.com>
2025-03-23 14:06:29 +0800

    a

--- train_vit_ldae_dino_self_dist_predictor.sh
+++ train_vit_ldae_dino_self_dist_predictor.sh
@@ -1,5 +1,5 @@
 
 
-python train_vit_ldae_dino_self_dist_predictor.py --decoder_embed_dim 192--use_amp --output_dir ../../autodl-tmp/output_tiny_self_dist_predictor/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train'
+python train_vit_ldae_dino_self_dist_predictor.py --decoder_embed_dim 192 --use_amp --output_dir ../../autodl-tmp/output_tiny_self_dist_predictor/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train'
 
 
