5ccd32cd8522856f046aa2ef195b936d36348a28
qqgeogor <308456341@qq.com>
2025-03-23 10:39:18 +0800

    a

---
+++ train_vit_ldae_dino.sh
@@ -0,0 +1,2 @@
+python train_vit_ldae_dino.py --use_amp --output_dir ../../autodl-tmp/vit_ldae_dino/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train' --num_views 2 --save_freq 20
+
