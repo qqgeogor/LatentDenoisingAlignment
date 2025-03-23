56e02c544b5a6fcf487fe293e69c2e8498ee24d2
qqgeogor <308456341@qq.com>
2025-03-23 10:41:14 +0800

    a

---
+++ train_mae_cifar10_jepa_dec_dino.sh
@@ -0,0 +1,2 @@
+python train_mae_cifar10_jepa_dec_dino.py --use_amp --output_dir ../../autodl-tmp/mae_cifar10_jepa_dec_dino/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train'  --save_freq 20
+
