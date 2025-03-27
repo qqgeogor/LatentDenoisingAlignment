548cd1771c96687846907c6a218da0027c2c9c8e
qqgeogor <30845634@qq.com>
2025-03-27 11:58:35 +0800

    a

---
+++ train_mae_cifar10_hldae_imagenet_sigma_0_333_sqrt.sh
@@ -0,0 +1,1 @@
+python train_mae_cifar10_hldae_imagenet_sigma_0_333_sqrt.py --use_amp --output_dir ../../autodl-tmp/output_ae_sigma_0_333_sqrt/  --img_size 128 --patch_size 16 --dataset 'tiny-imagenet' --data_path '../../autodl-tmp/tiny-imagenet-200/train' --save_freq 20
