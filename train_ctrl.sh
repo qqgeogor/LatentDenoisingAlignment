8732c43c725e1d6dc91994482525197dd90414f7
qqgeogor <308456341@qq.com>
2025-04-06 03:23:20 +0800

    a

--- train_ctrl.sh
+++ train_ctrl.sh
@@ -1,7 +1,7 @@
 python train_ctrl.py     \
     --data_path /mnt/d/datasets/cifar10     \
-    --output_dir /mnt/d/repo/output/cifar10-ebm-gan-r3gan-ctrl     \
+    --output_dir /mnt/d/repo/output/r3gan-ctrl     \
     --save_freq 20 \
-    --resume /mnt/d/repo/output/cifar10-ebm-gan-r3gan-ctrl/ebm_gan_checkpoint_180.pth
+    --resume /mnt/d/repo/output/r3gan-ctrl/ebm_gan_checkpoint_180.pth
 
 
