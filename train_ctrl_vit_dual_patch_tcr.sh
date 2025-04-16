9e6f5bd247693d8091d23e21f0ea728a2c211d9f
qqgeogor <308456341@qq.com>
2025-04-16 22:07:16 +0800

    a

--- train_ctrl_vit_dual_patch_tcr.sh
+++ train_ctrl_vit_dual_patch_tcr.sh
@@ -1,11 +1,20 @@
 
 
 python train_ctrl_vit_dual_patch_tcr.py     \
-    --data_path ../../autodl-tmp/cifar10     \
+    --dataset imagenet100 \
+    --data_path ../../autodl-tmp/imagenet100     \
     --output_dir ../../autodl-tmp/output_ctrl_vit_dual_patch_tcr/  \
-    --save_freq 20 \
+    --img_size 128 \
+    --patch_size 16 \
+    --embed_dim 192 \
+    --depth 12 \
+    --num_heads 3 \
+    --decoder_embed_dim 192 \
+    --decoder_depth 4 \
+    --decoder_num_heads 3 \
+    --save_freq 10 \
     --gp_weight 0.05 \
-    --adv_weight 0.5 \
+    --adv_weight 0.2 \
     --tcr_weight 0.0 \
-    --resume ../../autodl-tmp/output_ctrl_vit_dual_patch_tcr/ebm_gan_checkpoint_20.pth \
+    --global_weight 0.0 \
     --use_amp 
