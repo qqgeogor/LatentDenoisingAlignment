247a577a208bfe1568a3084c0daffcce9dcebeb4
qqgeogor <308456341@qq.com>
2025-04-17 11:35:39 +0800

    a

---
+++ train_ctrl_vit_dual_patch_tcr_local.sh
@@ -0,0 +1,21 @@
+
+python train_ctrl_vit_dual_patch_tcr.py     \
+    --dataset imagenet100 \
+    --data_path /mnt/d/datasets/imagenet100/train       \
+    --output_dir /mnt/d/repo/output/output_ctrl_vit_dual_patch_tcr/  \
+    --img_size 128 \
+    --patch_size 16 \
+    --embed_dim 192 \
+    --depth 12 \
+    --num_heads 3 \
+    --decoder_embed_dim 192 \
+    --decoder_depth 4 \
+    --decoder_num_heads 3 \
+    --save_freq 10 \
+    --gp_weight 0.05 \
+    --adv_weight 0.2 \
+    --tcr_weight 1 \
+    --global_weight 0.0 \
+    --temperature 1 \
+    --num_workers 8 \
+    --use_amp 
